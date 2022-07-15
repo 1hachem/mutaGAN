2ca9d6b4afad6279504c3739592a05a8194b5b6f
1hachem <hachem.betrouni@g.enp.edu.dz>
2022-07-15 23:23:43 +0100

    added gan training

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.model import Encoder, Decoder, Seq2Seq, Classifier
 from src.load_data import read_data, create_pairs, load_data, fast_load, save_loaders 
 from src.load_data import BiologicalSequenceDataset, collate_fn # used in case of is_fast_load=True
 from src.MLE_training import MLE_train
+from src.GAN_training import GAN_train
 from src.encode import to_ix
 
 from utils.utils import read_json
@@ -33,7 +34,7 @@ classifier = Classifier(params["encoder_hidden_size"], device=device)
 
 #optimizers
 optimizer_generator = Adam(seq2seq.parameters(), lr= params["MLE_learning_rate"])
-#optimizer_discriminator = Adam(classifier.parameters(), lr= params["GAN_learning_rate"])
+optimizer_discriminator = Adam(classifier.parameters(), lr= params["GAN_learning_rate"])
 
 #criterion
 MLE_criterion = nn.CrossEntropyLoss(ignore_index= to_ix["<pad>"])
@@ -51,4 +52,14 @@ else:
 
 
 #MLE training
-MLE_train(seq2seq, optimizer_generator, MLE_criterion, parent_data_loader, child_data_loader, num_epochs=params["MLE_num_epochs"], device=device)
+#MLE_train(seq2seq, optimizer_generator, MLE_criterion, parent_data_loader, child_data_loader, num_epochs=params["MLE_num_epochs"], device=device)
+
+
+#GAN training 
+seq2seq.set_teacher_forcing_ratio(params["GAN_teacher_forcing_ratio"]) #set teacher forcing to 0
+
+for g in optimizer_generator.param_groups: #changing the learning rate of the optimizer
+    g['lr'] = params["GAN_learning_rate"]
+
+GAN_train(seq2seq, encoder_disc, classifier, optimizer_generator, optimizer_discriminator, parent_data_loader, child_data_loader,
+                params["GAN_num_epochs"], device)
